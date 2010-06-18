90f980d34ca9c45fadc03026bd7e91e402f260cc
david <wmdaros@gmail.com>
2010-06-19 00:20:51 +0200

    Spring GuiRunner

---
+++ src/main/java/se/megalit/progress/SpringGuiRunner.java
@@ -0,0 +1,14 @@
+package se.megalit.progress;
+
+import javax.swing.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: David Rosell - Redpill-Linpro
+ * Date: Jun 19, 2010
+ * Time: 12:16:05 AM
+ */
+public class SpringGuiRunner {
+    private JButton startWorkButton;
+    private JTextField textField1;
+}
---
+++ src/main/java/se/megalit/progress/configuration/ApplicationConfiguration.java
@@ -0,0 +1,20 @@
+package se.megalit.progress.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import se.megalit.progress.worker.Worker;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: David Rosell - Redpill-Linpro
+ * Date: Jun 18, 2010
+ * Time: 11:54:41 PM
+ */
+@Configuration
+public class ApplicationConfiguration {
+
+    @Bean
+    public Worker worker() {
+        return new Worker();
+    }
+}
